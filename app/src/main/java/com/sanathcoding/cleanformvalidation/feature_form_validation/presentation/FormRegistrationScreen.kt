b96d45898d4bf6826dfcfca5ef465e6a81a73ee1
Sanath <sanath@entrusttitle.com>
2023-02-07 11:54:51 +0530

    Padding adjusted

--- app/src/main/java/com/sanathcoding/cleanformvalidation/feature_form_validation/presentation/FormRegistrationScreen.kt
+++ app/src/main/java/com/sanathcoding/cleanformvalidation/feature_form_validation/presentation/FormRegistrationScreen.kt
@@ -39,7 +39,7 @@ fun RegistrationFormScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(32.dp),
+            .padding(30.dp),
         verticalArrangement = Arrangement.Center
     ) {
         TextField(
